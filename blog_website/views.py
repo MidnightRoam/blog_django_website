ab5e7ade9b742c9d99c67dec3f61aadfcab87d17
MidnightRoam <saldatsenka@list.ru>
2022-07-20 15:36:53 +0300

    Added pagination

--- blog_website/views.py
+++ blog_website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
 
 from .models import Post
 
@@ -7,7 +8,12 @@ from .models import Post
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all()
-        return render(request, "blog_website/index.html", {"posts": posts})
+        paginator = Paginator(posts, 3)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, "blog_website/index.html", {"page_obj": page_obj})
 
 
 class AboutView(View):
