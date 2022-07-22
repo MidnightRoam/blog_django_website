6631839a2251b4fcf4925a7bcdde41fbbd2f9241
MidnightRoam <saldatsenka@list.ru>
2022-07-22 13:57:27 +0300

    Minor edits

--- blog_website/views.py
+++ blog_website/views.py
@@ -14,7 +14,7 @@ from .forms import RegisterForm, LoginForm, FeedBackForm, CommentForm
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all()
-        paginator = Paginator(posts, 3)
+        paginator = Paginator(posts, 6)
 
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
