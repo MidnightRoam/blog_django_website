9b43012dca10d8b2738511b62686be62fcbea5c7
MidnightRoam <saldatsenka@list.ru>
2022-07-22 12:57:40 +0300

    Added a comment system

--- blog_website/forms.py
+++ blog_website/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
+from .models import Comment
+
 
 class RegisterForm(forms.Form):
 
@@ -99,3 +101,16 @@ class FeedBackForm(forms.Form):
             "placeholder": "Message",
         })
     )
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('text', )
+        widgets = {
+            "text": forms.Textarea(attrs={
+                "class": "form-control",
+                "rows": 3
+            }),
+        }
